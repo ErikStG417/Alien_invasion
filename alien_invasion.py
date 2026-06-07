a48526630f53de82fa9237b84192d596952a4843
Erik <erik.stgeorges@yahoo.com>
2026-06-07 12:13:58 -0400

    fullscreen mode added with new pres 'q' to quit

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,9 +13,9 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
       
@@ -38,19 +38,25 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                  sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-                    
+                self.check_keydown_event(event)      
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self.check_keyup_event(event)
+                
+    def check_keydown_event(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
-            
-
+    def check_keyup_event(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
 
     def _update_screen(self):
--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ class Settings:
     def __init__(self):
         # initialize the game's settings
         #screen setting
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 850
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
+
+        # ship settings
+        self.ship_speed = 3.5
         
