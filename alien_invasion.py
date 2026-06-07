58ee23ba97d4dd3509fc1a5cd317761d877378ba
Erik <erik.stgeorges@yahoo.com>
2026-06-06 21:38:29 -0400

    added starting code for displaying the game

---
+++ alien_invasion.py
@@ -0,0 +1,41 @@
+import sys
+import pygame 
+
+from settings import Settings
+
+class AlienInvasion:
+    # Overall class to manage game assets and behavior.
+
+    def __init__(self):
+        # Initialize the game, and create game resource.
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.setting.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+
+    def run_game(self):
+        # start the main loop for the game
+        while True:
+            # watch for keywords and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit
+
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
